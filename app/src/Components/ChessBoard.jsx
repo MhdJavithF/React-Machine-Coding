f66a044f9ba4205bb6cc81c5ccaea3637de5cc84
MhdJavithF <javithmd001@gmail.com>
2025-08-19 06:55:54 +0530

    chess board css react

--- app/src/Components/ChessBoard.jsx
+++ app/src/Components/ChessBoard.jsx
@@ -4,18 +4,46 @@ import '../Styles/chess.css';
 const ChessBoard = () => {
     const size = 8;
     const cells = [];
+    const cells1 = [];
+    const cells2 = [];
+
 
     for (let row = 0; row < size; row++) {
         for (let col = 0; col < size; col++) {
             cells.push(
-                <div key={`${row}-${col}`} className="cell" style={{ backgroundColor: (row + col) % 2 === 0 ? "#fff" : "#000" }}></div>
+                <div key={`${row}-${col}`} className="cell" style={{ backgroundColor: (row + col) % 2 === 0 ? "#fff" : "#000" }}>{row}-{col}</div>
             )
         }
     }
+
+    for (let row = 0; row < size; row++) {
+        cells1[row] = [];
+        for (let col = 0; col < size; col++) {
+            cells1[row][col] = `${row}-${col}`;
+        }
+    }
+    console.log(cells1);
+    
+    let val = 0;
+    for(let i = 0; i < size; i++){
+        cells2[i] = [];
+        for(let j = 0; j < size; j++){
+            val = i + j;
+            if(val % 2 === 0){
+                cells2[i][j] = 'even'
+            }
+            else{
+                cells2[i][j] = 'odd'
+
+            }
+        }
+    }
+
+    console.log(cells2);
     
     return (
-        <div className='board'>
-            {cells}
+        <div>
+            <div className='board'>{cells}</div>
         </div>
     )
 }
